f55e3d462bdab169a55440bd84f7ff0e23ad37b4
JerySiri <jeryjavier@hotmail.com>
2021-02-20 15:56:48 -0400

    Capitulos 8 y 9

---
+++ Ejercicios_Del_Libro_CShart/Capitulo 6 y 7/Capitulo 6 y 7/Capitulos De Cshart.Designer.cs
@@ -0,0 +1,270 @@
+﻿
+namespace Capitulo_6_y_7
+{
+    partial class EjerciciosLibrosForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Capitulo6Label = new System.Windows.Forms.Label();
+            this.Capitulo7Label = new System.Windows.Forms.Label();
+            this.Ejercicio1_6Button = new System.Windows.Forms.Button();
+            this.Ejercicio2_6Button = new System.Windows.Forms.Button();
+            this.Ejercicio3_6Button = new System.Windows.Forms.Button();
+            this.Ejercicio4_6Button = new System.Windows.Forms.Button();
+            this.Ejercicio5_6Button = new System.Windows.Forms.Button();
+            this.Ejercicio1_7Button = new System.Windows.Forms.Button();
+            this.Ejercicio2_7Button = new System.Windows.Forms.Button();
+            this.Ejercicio5_7Button = new System.Windows.Forms.Button();
+            this.Ejercicio5_8Button = new System.Windows.Forms.Button();
+            this.Ejercicio3_8Button = new System.Windows.Forms.Button();
+            this.Capitulo8Label = new System.Windows.Forms.Label();
+            this.Ejercicio4_9Button = new System.Windows.Forms.Button();
+            this.Ejercicio3_9Button = new System.Windows.Forms.Button();
+            this.Ejercicio1_9Button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Capitulo6Label
+            // 
+            this.Capitulo6Label.AutoSize = true;
+            this.Capitulo6Label.Location = new System.Drawing.Point(68, 36);
+            this.Capitulo6Label.Name = "Capitulo6Label";
+            this.Capitulo6Label.Size = new System.Drawing.Size(61, 15);
+            this.Capitulo6Label.TabIndex = 0;
+            this.Capitulo6Label.Text = "Capitulo 6";
+            // 
+            // Capitulo7Label
+            // 
+            this.Capitulo7Label.AutoSize = true;
+            this.Capitulo7Label.Location = new System.Drawing.Point(68, 157);
+            this.Capitulo7Label.Name = "Capitulo7Label";
+            this.Capitulo7Label.Size = new System.Drawing.Size(61, 15);
+            this.Capitulo7Label.TabIndex = 1;
+            this.Capitulo7Label.Text = "Capitulo 7";
+            // 
+            // Ejercicio1_6Button
+            // 
+            this.Ejercicio1_6Button.Location = new System.Drawing.Point(68, 70);
+            this.Ejercicio1_6Button.Name = "Ejercicio1_6Button";
+            this.Ejercicio1_6Button.Size = new System.Drawing.Size(75, 23);
+            this.Ejercicio1_6Button.TabIndex = 2;
+            this.Ejercicio1_6Button.Text = "Ejercicio 1";
+            this.Ejercicio1_6Button.UseVisualStyleBackColor = true;
+            this.Ejercicio1_6Button.Click += new System.EventHandler(this.Ejercicio1_6Button_Click);
+            // 
+            // Ejercicio2_6Button
+            // 
+            this.Ejercicio2_6Button.Location = new System.Drawing.Point(68, 100);
+            this.Ejercicio2_6Button.Name = "Ejercicio2_6Button";
+            this.Ejercicio2_6Button.Size = new System.Drawing.Size(75, 23);
+            this.Ejercicio2_6Button.TabIndex = 3;
+            this.Ejercicio2_6Button.Text = "Ejercicio 2";
+            this.Ejercicio2_6Button.UseVisualStyleBackColor = true;
+            this.Ejercicio2_6Button.Click += new System.EventHandler(this.Ejercicio2_6Button_Click);
+            // 
+            // Ejercicio3_6Button
+            // 
+            this.Ejercicio3_6Button.Location = new System.Drawing.Point(149, 71);
+            this.Ejercicio3_6Button.Name = "Ejercicio3_6Button";
+            this.Ejercicio3_6Button.Size = new System.Drawing.Size(75, 23);
+            this.Ejercicio3_6Button.TabIndex = 4;
+            this.Ejercicio3_6Button.Text = "Ejercicio 3";
+            this.Ejercicio3_6Button.UseVisualStyleBackColor = true;
+            this.Ejercicio3_6Button.Click += new System.EventHandler(this.Ejercicio3_6Button_Click);
+            // 
+            // Ejercicio4_6Button
+            // 
+            this.Ejercicio4_6Button.Location = new System.Drawing.Point(149, 100);
+            this.Ejercicio4_6Button.Name = "Ejercicio4_6Button";
+            this.Ejercicio4_6Button.Size = new System.Drawing.Size(75, 23);
+            this.Ejercicio4_6Button.TabIndex = 5;
+            this.Ejercicio4_6Button.Text = "Ejercicio 4";
+            this.Ejercicio4_6Button.UseVisualStyleBackColor = true;
+            this.Ejercicio4_6Button.Click += new System.EventHandler(this.Ejercicio4_6Button_Click);
+            // 
+            // Ejercicio5_6Button
+            // 
+            this.Ejercicio5_6Button.Location = new System.Drawing.Point(230, 71);
+            this.Ejercicio5_6Button.Name = "Ejercicio5_6Button";
+            this.Ejercicio5_6Button.Size = new System.Drawing.Size(75, 23);
+            this.Ejercicio5_6Button.TabIndex = 6;
+            this.Ejercicio5_6Button.Text = "Ejercicio 5";
+            this.Ejercicio5_6Button.UseVisualStyleBackColor = true;
+            this.Ejercicio5_6Button.Click += new System.EventHandler(this.Ejercicio5_6Button_Click);
+            // 
+            // Ejercicio1_7Button
+            // 
+            this.Ejercicio1_7Button.Location = new System.Drawing.Point(68, 191);
+            this.Ejercicio1_7Button.Name = "Ejercicio1_7Button";
+            this.Ejercicio1_7Button.Size = new System.Drawing.Size(75, 23);
+            this.Ejercicio1_7Button.TabIndex = 7;
+            this.Ejercicio1_7Button.Text = "Ejercicio 1";
+            this.Ejercicio1_7Button.UseVisualStyleBackColor = true;
+            this.Ejercicio1_7Button.Click += new System.EventHandler(this.Ejercicio1_7Button_Click);
+            // 
+            // Ejercicio2_7Button
+            // 
+            this.Ejercicio2_7Button.Location = new System.Drawing.Point(68, 221);
+            this.Ejercicio2_7Button.Name = "Ejercicio2_7Button";
+            this.Ejercicio2_7Button.Size = new System.Drawing.Size(75, 23);
+            this.Ejercicio2_7Button.TabIndex = 8;
+            this.Ejercicio2_7Button.Text = "Ejercicio 2";
+            this.Ejercicio2_7Button.UseVisualStyleBackColor = true;
+            this.Ejercicio2_7Button.Click += new System.EventHandler(this.Ejercicio2_7Button_Click);
+            // 
+            // Ejercicio5_7Button
+            // 
+            this.Ejercicio5_7Button.Location = new System.Drawing.Point(149, 191);
+            this.Ejercicio5_7Button.Name = "Ejercicio5_7Button";
+            this.Ejercicio5_7Button.Size = new System.Drawing.Size(75, 23);
+            this.Ejercicio5_7Button.TabIndex = 9;
+            this.Ejercicio5_7Button.Text = "Ejercicio 5";
+            this.Ejercicio5_7Button.UseVisualStyleBackColor = true;
+            this.Ejercicio5_7Button.Click += new System.EventHandler(this.Ejercicio5_7Button_Click);
+            // 
+            // Ejercicio5_8Button
+            // 
+            this.Ejercicio5_8Button.Location = new System.Drawing.Point(68, 342);
+            this.Ejercicio5_8Button.Name = "Ejercicio5_8Button";
+            this.Ejercicio5_8Button.Size = new System.Drawing.Size(75, 23);
+            this.Ejercicio5_8Button.TabIndex = 12;
+            this.Ejercicio5_8Button.Text = "Ejercicio 5";
+            this.Ejercicio5_8Button.UseVisualStyleBackColor = true;
+            this.Ejercicio5_8Button.Click += new System.EventHandler(this.Ejercicio5_8Button_Click);
+            // 
+            // Ejercicio3_8Button
+            // 
+            this.Ejercicio3_8Button.Location = new System.Drawing.Point(68, 312);
+            this.Ejercicio3_8Button.Name = "Ejercicio3_8Button";
+            this.Ejercicio3_8Button.Size = new System.Drawing.Size(75, 23);
+            this.Ejercicio3_8Button.TabIndex = 11;
+            this.Ejercicio3_8Button.Text = "Ejercicio 3";
+            this.Ejercicio3_8Button.UseVisualStyleBackColor = true;
+            this.Ejercicio3_8Button.Click += new System.EventHandler(this.Ejercicio3_8Button_Click);
+            // 
+            // Capitulo8Label
+            // 
+            this.Capitulo8Label.AutoSize = true;
+            this.Capitulo8Label.Location = new System.Drawing.Point(68, 278);
+            this.Capitulo8Label.Name = "Capitulo8Label";
+            this.Capitulo8Label.Size = new System.Drawing.Size(61, 15);
+            this.Capitulo8Label.TabIndex = 10;
+            this.Capitulo8Label.Text = "Capitulo 8";
+            // 
+            // Ejercicio4_9Button
+            // 
+            this.Ejercicio4_9Button.Location = new System.Drawing.Point(149, 437);
+            this.Ejercicio4_9Button.Name = "Ejercicio4_9Button";
+            this.Ejercicio4_9Button.Size = new System.Drawing.Size(75, 23);
+            this.Ejercicio4_9Button.TabIndex = 16;
+            this.Ejercicio4_9Button.Text = "Ejercicio 4";
+            this.Ejercicio4_9Button.UseVisualStyleBackColor = true;
+            this.Ejercicio4_9Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Ejercicio3_9Button
+            // 
+            this.Ejercicio3_9Button.Location = new System.Drawing.Point(68, 467);
+            this.Ejercicio3_9Button.Name = "Ejercicio3_9Button";
+            this.Ejercicio3_9Button.Size = new System.Drawing.Size(75, 23);
+            this.Ejercicio3_9Button.TabIndex = 15;
+            this.Ejercicio3_9Button.Text = "Ejercicio 3";
+            this.Ejercicio3_9Button.UseVisualStyleBackColor = true;
+            this.Ejercicio3_9Button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Ejercicio1_9Button
+            // 
+            this.Ejercicio1_9Button.Location = new System.Drawing.Point(68, 437);
+            this.Ejercicio1_9Button.Name = "Ejercicio1_9Button";
+            this.Ejercicio1_9Button.Size = new System.Drawing.Size(75, 23);
+            this.Ejercicio1_9Button.TabIndex = 14;
+            this.Ejercicio1_9Button.Text = "Ejercicio 1";
+            this.Ejercicio1_9Button.UseVisualStyleBackColor = true;
+            this.Ejercicio1_9Button.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 403);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Capitulo 9";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // EjerciciosLibrosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 605);
+            this.Controls.Add(this.Ejercicio4_9Button);
+            this.Controls.Add(this.Ejercicio3_9Button);
+            this.Controls.Add(this.Ejercicio1_9Button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Ejercicio5_8Button);
+            this.Controls.Add(this.Ejercicio3_8Button);
+            this.Controls.Add(this.Capitulo8Label);
+            this.Controls.Add(this.Ejercicio5_7Button);
+            this.Controls.Add(this.Ejercicio2_7Button);
+            this.Controls.Add(this.Ejercicio1_7Button);
+            this.Controls.Add(this.Ejercicio5_6Button);
+            this.Controls.Add(this.Ejercicio4_6Button);
+            this.Controls.Add(this.Ejercicio3_6Button);
+            this.Controls.Add(this.Ejercicio2_6Button);
+            this.Controls.Add(this.Ejercicio1_6Button);
+            this.Controls.Add(this.Capitulo7Label);
+            this.Controls.Add(this.Capitulo6Label);
+            this.Name = "EjerciciosLibrosForm";
+            this.Text = "Capitulos del Libro de CShart";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Capitulo6Label;
+        private System.Windows.Forms.Label Capitulo7Label;
+        private System.Windows.Forms.Button Ejercicio1_6Button;
+        private System.Windows.Forms.Button Ejercicio2_6Button;
+        private System.Windows.Forms.Button Ejercicio3_6Button;
+        private System.Windows.Forms.Button Ejercicio4_6Button;
+        private System.Windows.Forms.Button Ejercicio5_6Button;
+        private System.Windows.Forms.Button Ejercicio1_7Button;
+        private System.Windows.Forms.Button Ejercicio2_7Button;
+        private System.Windows.Forms.Button Ejercicio5_7Button;
+        private System.Windows.Forms.Button Ejercicio5_8Button;
+        private System.Windows.Forms.Button Ejercicio3_8Button;
+        private System.Windows.Forms.Label Capitulo8Label;
+        private System.Windows.Forms.Button Ejercicio4_9Button;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Ejercicio1_9Button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.Button Ejercicio3_9Button;
+    }
+}
+
